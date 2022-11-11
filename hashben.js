2f8a5a8a9c1e6ac58291234d5c38b2b4b895de79
hexuben <109147909+hexuben@users.noreply.github.com>
2022-11-11 13:12:06 +0800

    Delete hashben.js

--- hashben.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    if(localStorage.maincolor==undefined)
-        localStorage.maincolor="grey-900";
-    var maincolor=localStorage.maincolor;
-    $('body').addClass("mdui-theme-primary-"+maincolor);
-});
