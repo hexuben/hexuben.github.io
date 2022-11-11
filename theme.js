b2cba24d6748e65e11c9c5722dddfd19fd62135c
hexuben <109147909+hexuben@users.noreply.github.com>
2022-11-11 13:49:44 +0800

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,3 +1,8 @@
+function changeTheme(theme) {
+    $('body').removeClass("mdui-theme-primary-" + localStorage.maincolor);
+	localStorage.maincolor = theme;
+    $('body').addClass("mdui-theme-primary-" + localStorage.maincolor);
+}
 $(document).ready(function(){
     if(localStorage.maincolor==undefined)
         localStorage.maincolor="deep-purple";
